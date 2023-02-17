4af3327679c7e1ee1651df4b65e25d37b6e7bace
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-17 20:39:46 +0300

    Добавил задание #1

---
+++ HomeWork9.Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork9.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception myException = new Exception("Мое исключение!");
+
+            var exceptionList = new Exception[] { myException, new ArgumentException(), new NotImplementedException(), new ArithmeticException(), new ArgumentOutOfRangeException() };
+
+            foreach (var ex in exceptionList)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Сработало исключение: {e.GetType()}, {e.Message}");
+                }
+                finally
+                {
+                    // Ждем нажатия Enter, чтобы перейти к следующему исключению
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
